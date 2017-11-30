25d7bfcd25f0bceda622b1919efbfae3c392d342
Helmy <helmy2009@gmail.com>
2017-12-01 00:01:20 +0700

    Remove posts format

--- functions.php
+++ functions.php
@@ -23,9 +23,9 @@ function wisataidaman_setup() {
 		'search-form','comment-form','comment-list','gallery','caption',
 	) );
 
-	add_theme_support( 'post-formats', array(
-		'aside','image','video','quote','link','gallery','status','audio','chat',
-	) );
+	//add_theme_support( 'post-formats', array(
+		//'aside','image','video','quote','link','gallery','status','audio','chat',
+	//) );
 
 }
 endif;
